ffc7c0f32e7bc1ea0fc1a95543a180d98fb74085
Yash <yashagarwal827327@gmail.com>
2024-01-07 23:52:40 +0530

    test

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ async def login_for_access_token(response: Response, form_data: OAuth2PasswordRe
     cookie_expires = datetime.utcnow()+access_token_expires
     expires_formatted = cookie_expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
-    response.set_cookie(key="token", value=access_token, expires=expires_formatted, httponly=True, secure=True)
+    response.set_cookie(key="token", value=access_token, expires=expires_formatted, httponly=True, secure=True, samesite="none")
     return {"message": "cookie set"}
 
 
