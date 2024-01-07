e154a758c0dc761fb57cad71319ad4b97eaee854
Yash <yashagarwal827327@gmail.com>
2024-01-07 17:42:22 +0530

    learning

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://127.0.0.1:5500"],  # Replace with the origin of your frontend
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
